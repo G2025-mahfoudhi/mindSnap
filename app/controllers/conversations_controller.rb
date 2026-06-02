9e431f7e1a0740e78608c951ce0f1cff83fd807e
thierry praud <praud.thierry@wanadoo.fr>
2026-06-02 10:10:07 +0200

    Controller create

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,4 +2,21 @@ class ConversationsController < ApplicationController
   def index
     @conversations = current_user.conversations
   end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+    @conversation.user = current_user
+
+    if @conversation.save
+      redirect_to conversation_path(@conversation)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:name)
+  end
 end
