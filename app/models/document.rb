30d24278c05d36e65a09a8fb147151e9992bb976
Mahfoudhi Ghzela <ghzelamahfoudhi@yahoo.fr>
2026-06-03 17:33:42 +0200

    model document before save

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,4 +6,16 @@ class Document < ApplicationRecord
 
   validates :title, presence: true
   validates :document_type, presence: true
+
+  before_save :set_pdf_resource_type
+
+  private
+
+  def set_pdf_resource_type
+    file.each do |attachment|
+      if attachment.blob.content_type == "application/pdf"
+        attachment.blob.metadata["resource_type"] = "raw"
+      end
+    end
+  end
 end
