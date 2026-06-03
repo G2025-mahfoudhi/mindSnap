016417ca77904f5e04acc22e44305cb56a6b3792
Mahfoudhi Ghzela <ghzelamahfoudhi@yahoo.fr>
2026-06-03 17:54:33 +0200

    initializer cloudinary fixed

--- config/initializers/cloudinary_fix_pdf.rb
+++ config/initializers/cloudinary_fix_pdf.rb
@@ -1,28 +1,32 @@
+# config/initializers/cloudinary_pdf_fix.rb
 Rails.application.config.after_initialize do
-  ActiveStorage::Service::CloudinaryService.class_eval do
-    private
+  require 'active_storage/service/cloudinary_service'
 
-    def content_type_to_resource_type(content_type)
-      return 'image' if content_type.nil?
+  ActiveStorage::Service::CloudinaryService.class_eval do
+    private
 
-      type, subtype = content_type.split('/')
-      case type
-      when 'video', 'audio'
-        'video'
-      when 'text', 'message'
-        'raw'
-      when 'application'
-        case subtype
-        when 'pdf', 'postscript'
-          'raw'  # <-- le fix
-        when 'vnd.apple.mpegurl', 'x-mpegurl', 'mpegurl'
-          'video'
-        else
-          'raw'
-        end
-      else
-        'image'
-      end
-    end
-  end
+    def content_type_to_resource_type(content_type)
+      return 'image' if content_type.nil?
+
+      type, subtype = content_type.split('/')
+      case type
+      when 'video', 'audio'
+        'video'
+      when 'text', 'message'
+        'raw'
+      when 'application'
+        case subtype
+        when 'pdf', 'postscript'
+          'raw'
+        when 'vnd.apple.mpegurl', 'x-mpegurl', 'mpegurl'
+          'video'
+        else
+          'raw'
+        end
+      else
+        'image'
+      end
+    end
+  end
+end
 end
