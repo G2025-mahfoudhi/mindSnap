2e726884675b5e1fd312baf666a20ec872fecf0b
thierry praud <praud.thierry@wanadoo.fr>
2026-06-08 15:29:25 +0200

    sauvegarde oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ApplicationRecord
       user.first_name = auth.info.first_name
       user.last_name  = auth.info.last_name
       user.password   = Devise.friendly_token[0, 20]
+      # goo_avatar = auth.info.image
     end
   end
 end
