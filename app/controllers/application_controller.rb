3de21020ab65090265831cbd5303f612c842ccc7
Mahfoudhi Ghzela <ghzelamahfoudhi@yahoo.fr>
2026-06-08 15:00:01 +0200

    photo de profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,9 +45,9 @@ class ApplicationController < ActionController::Base
   # grâce au before_action :configure_permitted_parameters.
   def configure_permitted_parameters
     # Champs autorisés à l'inscription (sign_up)
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name avatar])
 
     # Champs autorisés à la modification du profil (account_update)
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name avatar])
   end
 end
