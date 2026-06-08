8ee9c5e200a70c33875c86ddf039db5677b7e111
thierry praud <praud.thierry@wanadoo.fr>
2026-06-08 10:19:19 +0200

    sauvegarde

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_05_102816) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_08_081555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vector"
@@ -271,9 +271,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_102816) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "provider"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.string "uid"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
