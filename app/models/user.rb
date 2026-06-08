7a0ae261e08fb98970192a7a01c19e3fdb7f40e3
thierry praud <praud.thierry@wanadoo.fr>
2026-06-08 12:19:23 +0200

    sauvegarde oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,17 @@ class User < ApplicationRecord
   # validates :first_name, presence: true
   # validates :last_name, presence: true
   # validates :email, uniqueness: { scope: :password }
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         # for Google OmniAuth
+         :omniauthable, omniauth_providers:[:google_oauth2]
+
+  def self.from_omniauth(auth)
+    # Find or create a user based on the provider and uid
+    where(provider:auth.provider, uid:auth.uid).first_or_initialize do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20] # Generate a random password
+    end
+  end
 end
