52113d43c8ba9ffb6dbdb609c970c7326564d305
Mahfoudhi Ghzela <ghzelamahfoudhi@yahoo.fr>
2026-06-09 14:31:41 +0200

    dashboard_fix

--- app/javascript/controllers/pricing_toggle_controller.js
+++ app/javascript/controllers/pricing_toggle_controller.js
@@ -1,16 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
+const STORAGE_KEY = "mindsnap_billing_period"
+
 export default class extends Controller {
   static targets = ["toggle", "proAmount", "proSub", "businessAmount", "businessSub", "savings"]
 
   connect() {
-    this.showMonthly()
+    const saved = localStorage.getItem(STORAGE_KEY)
+    if (saved === "annual") {
+      this.toggleTarget.checked = true
+      this.showAnnual()
+    } else {
+      this.toggleTarget.checked = false
+      this.showMonthly()
+    }
   }
 
   switch() {
     if (this.toggleTarget.checked) {
+      localStorage.setItem(STORAGE_KEY, "annual")
       this.showAnnual()
     } else {
+      localStorage.setItem(STORAGE_KEY, "monthly")
       this.showMonthly()
     }
   }
